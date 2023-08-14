b078d4c7e865df28231334c578523aa678447850
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2023-08-14 10:12:31 +0200

    Add stubs for algo API

---
+++ app/api/v1/hashing.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+
+router = APIRouter(
+    prefix="/hashing",
+    tags=["Hashing"]
+)
+
+
+class RequestBody(BaseModel):
+    pass
+
+
+class ResponseBody(BaseModel):
+    pass
+
+
+@router.get(
+    "/",
+    response_model=ResponseBody,
+    summary="This is my summary",
+    description="This is my description",
+    response_description="This is my response description")
+async def endpoint(requestBody: RequestBody):
+    return requestBody
