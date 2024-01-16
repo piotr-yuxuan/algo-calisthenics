2860e778c0f3ea762a247a422650542df5ad335b
Walter CI <piotr-yuxuan@users.noreply.github.com>
2024-01-16 18:33:06 +0000

    Update dependency versions

--- project.clj
+++ project.clj
@@ -14,7 +14,7 @@
   :dependencies [[camel-snake-kebab "0.4.3"]
                  [clj-time "0.15.2"]
                  [instaparse "1.4.12"]
-                 [metosin/malli "0.13.0"]
+                 [metosin/malli "0.14.0"]
                  [org.clojure/data.csv "1.0.1"]
                  [org.clojure/clojure "1.12.0-alpha5"]]
   :profiles {:github {:github/topics ["kata" "playground" "algorithm" "practice" "code"]
