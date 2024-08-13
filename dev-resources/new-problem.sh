b6873795d21f97b7f13a8db390e6f8eb8bb7a1cf
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2024-08-13 18:06:32 +0200

    Automate file creation from new problem

---
+++ dev-resources/new-problem.sh
@@ -0,0 +1,91 @@
+#!/bin/zsh
+
+set -euo pipefail
+
+SHORT_OPTS="hvp:f"
+LONG_OPTS="help,verbose,problem-path:,force"
+
+PARSED_OPTS=$(getopt \
+	      --options $SHORT_OPTS \
+	      --longoptions $LONG_OPTS \
+	      --name "$0" -- "$@")
+[[ $? -ne 0 ]] && print -P "%F{red}Error from $(where getopt).%f" >&2 && exit 1
+eval set -- "$PARSED_OPTS"
+
+verbose=0
+problem_path=""
+force_flag=""
+DIR=$(dirname $(realpath $0))
+
+function print_help {
+  echo "Usage: $0 [-h|--help] [-v|--verbose] [-p|--problem-path <path>]"
+  echo ""
+  echo "Options:"
+  echo "  -h, --help            Show this help message and exit"
+  echo "  -v, --verbose         Enable verbose output"
+  echo "  -p, --problem-path    Specify the problem path (mandatory)"
+  echo "  -f, --force           Overwrite file if exists"
+}
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -h | --help)
+      print_help()
+      exit 0
+      ;;
+    -v | --verbose)
+      (( verbose = verbose + 1 ))
+      shift
+      ;;
+    -p | --problem-path)
+      problem_path="$2"
+      shift 2
+      ;;
+    -f | --force)
+      force_flag=1
+      shift 2
+      ;;
+    --)
+      shift
+      break
+      ;;
+    *)
+      echo "Unknown option: $1"
+      exit 1
+      ;;
+  esac
+done
+
+if [[ -z "$problem_path" ]]; then
+  echo "Error: --problem-path is required"
+  print_help
+  exit 1
+fi
+
+
+src_paths=(
+  "${DIR}/problem_template.py"
+  "${DIR}/problem_template.org"
+  "${DIR}/problem_template_test.py"
+)
+dest_paths=(
+  "${DIR}/../src/${problem_path}.py"
+  "${DIR}/../src/${problem_path}.org"
+  "${DIR}/../tests/${problem_path}_test.py"
+)
+
+for i in {1..3}; do
+  src=${src_paths[$i]}
+  dest=$(realpath "${dest_paths[$i]}")
+
+  if [[ -e $dest && $force_flag -eq 0 ]]; then
+    print -P "%F{red}Error: File $dest already exists.\n%f" >&2
+    print_help
+    exit 1
+  fi
+
+  cp "$src" "$dest"
+  if [[ -n ${verbose} ]]; then
+    echo "Copied $src to $dest"
+  fi
+done
