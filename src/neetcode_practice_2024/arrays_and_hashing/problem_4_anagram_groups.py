edf89d9e50a56cd19799e0d7a37407b83304f60a
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2024-08-15 00:04:36 +0200

    Neetcode arrays problem 4: anagram groups

--- src/neetcode_practice_2024/arrays_and_hashing/problem_4_anagram_groups.py
+++ src/neetcode_practice_2024/arrays_and_hashing/problem_4_anagram_groups.py
@@ -3,8 +3,76 @@
 import argparse
 import argcomplete
 
-from typing import List
+from typing import Dict, Generic, List, Optional, TypeVar
 import functools
+from collections import defaultdict
+
+
+def solution_naive(input: List[str]):
+    acc = defaultdict(list)
+    for s in input:
+        acc["".join(sorted(s))].append(s)
+    return list(acc.values())
+
+
+def solution_character_counting(input: List[str]):
+    acc = defaultdict(list)
+    for s in input:
+        a = [0] * 26
+        for i in s:
+            if not ord("a") <= ord(i) <= ord("z"):
+                raise ValueError(
+                    f"Unexpected character {i} with integer value of {ord(i)}."
+                )
+            a[ord(i) - ord("a")] += 1
+        acc[tuple(a)].append(s)
+    return list(acc.values())
+
+
+K = TypeVar("K")
+V = TypeVar("V")
+
+
+class Node(Generic[K, V]):
+    def __init__(self):
+        self.children: Dict[K, "Node[K, V]"] = defaultdict(Node)
+        self.values: List[V] = list()
+
+    def __repr__(self) -> str:
+        default_repr = object.__repr__(self)
+        return f"{default_repr} {{children: {self.children.keys()}, values: {self.values}}}"
+
+
+class Trie(Generic[K, V]):
+    def __init__(self):
+        self.root: Node[K, V] = Node()
+
+    def node_at(self, path: List[K]) -> Node[K, V]:
+        return functools.reduce(lambda node, k: node.children[k], path, self.root)
+
+    def insert(self, path: List[K], value: V):
+        self.node_at(path).values.append(value)
+
+    def values_at(self, path: List[K]) -> List[V]:
+        return self.node_at(path).values
+
+    def values_under(self, path: List[K]):
+        result: List[V] = []
+        self._traverse(self.node_at(path), result)
+        return result
+
+    def _traverse(self, node: Node[K, V], result: List[V]):
+        if node.values:  # not empty
+            result.append(node.values)
+        for _, n in node.children.items():
+            self._traverse(n, result)
+
+
+def solution_trie_based(input: List[str]):
+    t = Trie()
+    for s in input:
+        t.insert(sorted(s), s)
+    return t.values_under([])
 
 
 def solution(input: List[str]):
--- tests/neetcode_practice_2024/arrays_and_hashing/problem_4_anagram_groups_test.py
+++ tests/neetcode_practice_2024/arrays_and_hashing/problem_4_anagram_groups_test.py
@@ -2,15 +2,71 @@ from hypothesis import given, strategies as st
 import neetcode_practice_2024.arrays_and_hashing.problem_4_anagram_groups as problem
 import itertools
 
+import string
+import importlib
 
-@given(st.sets(st.text(min_size=1, max_size=5), min_size=1, max_size=5))
+importlib.reload(problem)
+
+
+@given(
+    st.sets(
+        st.text(
+            alphabet=st.characters(min_codepoint=ord("a"), max_codepoint=ord("z")),
+            min_size=1,
+            max_size=5,
+        ),
+        min_size=1,
+        max_size=5,
+    )
+)
 def test_solution(l):
     input = ["".join(x) for s in l for x in list(itertools.permutations(s))]
     expected = [
         sorted(["".join(x) for x in list(itertools.permutations(s))]) for s in l
     ]
-    actual = [sorted(i) for i in problem.solution(input)]
-
     expected.sort()
+
+    actual = [sorted(i) for i in problem.solution(input)]
     actual.sort()
+
+    actual2 = [sorted(i) for i in problem.solution_naive(input)]
+    actual2.sort()
+
+    actual3 = [sorted(i) for i in problem.solution_character_counting(input)]
+    actual3.sort()
+
+    actual4 = [sorted(i) for i in problem.solution_trie_based(input)]
+    actual4.sort()
+
     assert expected == actual
+    assert expected == actual2
+    assert expected == actual3
+    assert expected == actual4
+
+
+def test_trie_node():
+    node = problem.Node()
+    assert isinstance(node, problem.Node)
+    assert isinstance(node.children["_"], problem.Node)
+    node.values.append(1)
+    assert node.values == [1]
+
+
+def test_trie():
+    t = problem.Trie()
+    for path, v in [
+        (
+            list(range(i)),
+            string.ascii_lowercase[i],
+        )
+        for i in range(len(string.ascii_lowercase))
+    ]:
+        t.insert(path, v)
+    for path, values in [
+        (
+            list(range(i)),
+            list(map(lambda x: [x], string.ascii_lowercase[i:])),
+        )
+        for i in range(len(string.ascii_lowercase))
+    ]:
+        assert t.values_under(path) == values
