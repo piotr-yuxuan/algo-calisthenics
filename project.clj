af2758e10d77bd249b5c8faa7d6c3ae5daa19f6b
Walter CI <piotr-yuxuan@users.noreply.github.com>
2024-08-28 18:38:28 +0000

    Update dependency versions

--- project.clj
+++ project.clj
@@ -16,7 +16,7 @@
                  [instaparse "1.5.0"]
                  [metosin/malli "0.16.3"]
                  [org.clojure/data.csv "1.1.0"]
-                 [org.clojure/clojure "1.12.0-rc1"]]
+                 [org.clojure/clojure "1.12.0-rc2"]]
   :profiles {:github {:github/topics ["kata" "playground" "algorithm" "practice" "code"]
                       :github/private? false}
              :provided {:dependencies []}
