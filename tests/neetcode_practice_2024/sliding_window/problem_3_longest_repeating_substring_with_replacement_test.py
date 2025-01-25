9e10d7f93042e8e5084ab9000bdfc4d7899ebd9c
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2025-01-25 22:24:24 +0000

    Fix naming practice: problem ordinal

--- tests/neetcode_practice_2024/sliding_window/problem_3_longest_repeating_substring_with_replacement_test.py
+++ tests/neetcode_practice_2024/sliding_window/problem_3_longest_repeating_substring_with_replacement_test.py
@@ -1,15 +1,14 @@
 from hypothesis import given, strategies as st
-import neetcode_practice_2024.sliding_window.longest_repeating_substring_with_replacement as problem
+import neetcode_practice_2024.sliding_window.problem_3_longest_repeating_substring_with_replacement as problem
 import random
 import itertools
 import string
 
 import importlib
+
 importlib.reload(problem)
 
 
 @given(st.text(min_size=1))
 def test_solution(input):
-    assert (
-        problem.solution(input) is True
-    )
+    assert problem.solution(input) is True
