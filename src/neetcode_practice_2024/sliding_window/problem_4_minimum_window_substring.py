1f6bf078eb3e8fcc259ac9da6d28b9b131f7e175
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2025-01-27 15:59:43 +0000

    Generated files for sliding_window/problem_4

---
+++ src/neetcode_practice_2024/sliding_window/problem_4_minimum_window_substring.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import argparse
+import argcomplete
+
+from typing import Callable, List, Optional, Protocol, TypeVar
+import collections
+import functools
+
+
+def solution(input):
+    return True
+
+
+def main(args):
+    return solution(args.input)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description=", ".join(
+            "Neetcode 2024",
+            "XXX",
+            "problem XXX.",
+        )
+    )
+    parser.add_argument(
+        "--input",
+        "-i",
+        type=lambda s: [int(i) for i in s.split(",")],
+        required=True,
+        help="The input array as a comma-separated list of integers: `1,2,3`.",
+    )
+
+    # https://kislyuk.github.io/argcomplete/#installation
+    argcomplete.autocomplete(parser)
+
+    args = parser.parse_args()
+    print(main(args))
+    exit(0)
