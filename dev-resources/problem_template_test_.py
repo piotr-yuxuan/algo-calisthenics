711345ba9195532029a86c3b97ccf4184d18211c
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2025-01-31 14:58:04 +0100

    Fix problem test template formatting

--- dev-resources/problem_template_test_.py
+++ dev-resources/problem_template_test_.py
@@ -5,11 +5,10 @@ import itertools
 import string
 
 import importlib
+
 importlib.reload(problem)
 
 
 @given(st.text(min_size=1))
 def test_solution(input):
-    assert (
-        problem.solution(input) is True
-    )
+    assert problem.solution(input) is True
