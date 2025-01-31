2560f6cd13b6cbf9d6f298c1bf56b1c046880493
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2025-01-31 15:09:33 +0100

    Generated files for dp1/problem_7

---
+++ tests/neetcode_practice_2024/dp1/problem_7_coin_change_test.py
@@ -0,0 +1,14 @@
+from hypothesis import given, strategies as st
+import neetcode_practice_2024.dp1.problem_7_coin_change as problem
+import random
+import itertools
+import string
+
+import importlib
+
+importlib.reload(problem)
+
+
+@given(st.text(min_size=1))
+def test_solution(input):
+    assert problem.solution(input) is True
