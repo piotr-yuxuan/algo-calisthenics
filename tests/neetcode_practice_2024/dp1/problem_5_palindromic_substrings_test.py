e6c81feaf2856f5b8ee6f1468e09c5a8bd647dae
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2025-01-31 09:28:35 +0100

    Generated files for dp1/problem_5

---
+++ tests/neetcode_practice_2024/dp1/problem_5_palindromic_substrings_test.py
@@ -0,0 +1,15 @@
+from hypothesis import given, strategies as st
+import neetcode_practice_2024.dp1.problem_5_palindromic_substrings as problem
+import random
+import itertools
+import string
+
+import importlib
+importlib.reload(problem)
+
+
+@given(st.text(min_size=1))
+def test_solution(input):
+    assert (
+        problem.solution(input) is True
+    )
