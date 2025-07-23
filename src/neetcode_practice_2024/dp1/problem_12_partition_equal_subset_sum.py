c4dc69cd9b9701edda3a870ec55def0d46aa5980
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2025-07-23 23:02:45 +0100

    Add commentary and solutions for problem 12

--- src/neetcode_practice_2024/dp1/problem_12_partition_equal_subset_sum.py
+++ src/neetcode_practice_2024/dp1/problem_12_partition_equal_subset_sum.py
@@ -1,18 +1,38 @@
 #!/usr/bin/env python
 
-import numpy as np
-import numpy.linalg as linalg
-
 import argparse
 import argcomplete
 
-from typing import Callable, List, Optional, Protocol, TypeVar
-import collections
-import functools
+from typing import List
 
 
-def solution(input):
-    return True
+def solution_perfect(input: List[int]) -> bool:
+    total = sum(input)
+    if total & 1:
+        return False
+
+    target = total >> 1
+
+    bits = 1  # bitmask: bits[i] == 1 means sum 'i' is possible
+    for x in input:
+        bits |= bits << x
+        if (bits >> target) & 1:
+            return True
+    return False
+
+
+def solution_legible(input: List[int]) -> bool:
+    target = sum(input) / 2
+    if int(target) != target:
+        return False
+
+    known_sums = set({})
+    for x in input:
+        known_sums = known_sums.union({x + y for y in known_sums})
+        known_sums.add(x)
+        if target in known_sums:
+            return True
+    return False
 
 
 def main(args):
