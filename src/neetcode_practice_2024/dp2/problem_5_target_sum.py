945dd6445de14f74097ab417a7f8a1c5a5be890e
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2025-07-26 20:52:06 +0100

    Update solution for dp2/problem_5_target_sum
    https://leetcode.com/submissions/detail/1709965842/

--- src/neetcode_practice_2024/dp2/problem_5_target_sum.py
+++ src/neetcode_practice_2024/dp2/problem_5_target_sum.py
@@ -5,10 +5,55 @@ import argcomplete
 
 from typing import List
 import functools
+import collections
+
+
+def solution_tabular_pruning(nums: List[int], target: int) -> int:
+    diff = sum(nums) - target
+    if diff < 0 or 1 == diff % 2:
+        return 0
+
+    # previous, current
+    p = dict({0: 1})
+
+    for n in nums:
+        c = dict({})
+        for j, v in p.items():
+            c[j + n] = c.get(j + n, 0) + v
+            c[j - n] = c.get(j - n, 0) + v
+        p = c
+    return c.get(target, 0)
+
+
+def solution_dict_space_optimised(self, nums: List[int], target: int) -> int:
+    # previous, current
+    p = collections.defaultdict(int)
+    p[0] = 1
+    c = collections.defaultdict(int)
+
+    for i in range(len(nums)):
+        c = collections.defaultdict(int)
+        for j in p.keys():
+            c[j + nums[i]] += p[j]
+            c[j - nums[i]] += p[j]
+        p = c
+    return c[target]
+
+
+def solution_standard_bottom_up(self, nums: List[int], target: int) -> int:
+    # Number of ways to reach this results from the prior subarray.
+    dp = [collections.defaultdict(int) for _ in range(len(nums) + 1)]
+    dp[0][nums[0]] = 1
+    dp[0][-nums[0]] += 1
+
+    for i in range(1, len(nums)):
+        for j in dp[i - 1].keys():
+            dp[i][j + nums[i]] += dp[i - 1][j]
+            dp[i][j - nums[i]] += dp[i - 1][j]
+    return dp[len(nums) - 1][target]
 
 
 def solution(nums: List[int], target: int) -> int:
-
     @functools.cache
     def dfs(i, current):
 
